6d0c66de9b67dba5fc6e1b95b2790ce3d16dee94
takahashiyasuh <takahashiyasuh@gmail.com>
2020-06-16 18:57:52 +0900

    #14 書き方の学習を行った。

---
+++ thread1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void DelegateUpdateText(string message, int i);
+
+        private void OnUpdateText(string message, int i)
+        {
+            this.textBox1.Text += (message + i + Environment.NewLine);
+        }
+
+        private void Work()
+        {
+            string message = "test ";
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+
+                this.textBox1.Invoke(new DelegateUpdateText(OnUpdateText), new object[] { message, i });
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            //Taskクラスの引数にActionを指定して生成する
+            Task task = new Task(Work);
+
+            //Taskを開始する
+            task.Start();
+            
+            /*
+            //これでもOK
+            Task task = Task.Run(() => {
+                string message = "test ";
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(1000);
+
+                    this.textBox1.Invoke(new DelegateUpdateText(OnUpdateText), new object[] { message, i });
+                }
+            });
+            */
+        }
+    }
+}
---
+++ thread1/WindowsFormsApp1/Form4.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        /* MethodInvokerを利用することで delegate が不要になった。
+        private delegate void DelegateUpdateText(string message, int i);
+        */
+
+        private void OnUpdateText(string message, int i)
+        {
+            this.textBox1.Text += (message + i + Environment.NewLine);
+        }
+
+        /* Workの内容はラムダ式で記述できる。
+        private void Work()
+        {
+            string message = "test ";
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+
+                this.textBox1.Invoke(new DelegateUpdateText(OnUpdateText), new object[] { message, i });
+            }
+        }
+        */
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+
+            //引数をラムダ式で作成する
+            Task task = new Task(() => {
+                string message = "test ";
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(1000);
+
+                    //MethodInvokerにキャストすることで、delegate関数が不要になる。
+                    this.textBox1.Invoke((MethodInvoker)delegate ()
+                    {
+                        OnUpdateText(message, i);
+                        
+                        //ここまで来ると以下のように記述できるのでメソッド（OnUpdateText）も必要がなくなりスッキリする。
+                        //this.textBox1.Text += (message + i + Environment.NewLine);
+                    });
+                }
+            });
+            //Taskを開始する
+            task.Start();
+            /*
+            //これでもOK
+            Task task = Task.Run(() => {
+                string message = "test ";
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(1000);
+
+                    //MethodInvokerにキャストすることで、delegate関数が不要になる。
+                    this.textBox1.Invoke((MethodInvoker)delegate ()
+                    {
+                        OnUpdateText(message, i);
+                    });
+                }
+            });
+            */
+        }
+    }
+}
